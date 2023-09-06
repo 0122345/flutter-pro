31502452009a9c7ee62cbe72240f30bc1bb6b5d4
0112345 <ntwarifiacre043@gmail.com>
2023-09-06 18:42:40 +0200

    initial commit

---
+++ anime/lib/learn_flutter.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('learn flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('image/renguko.jpg'),
+          const Divider(
+            color : Colors.black,
+          )
+        ],
+      )
+    );
+  }
+}
